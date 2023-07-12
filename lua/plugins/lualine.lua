46deec265417f7d30a8917a4aa9d6e99b4c94b6c
Benoit Varillon <benoit.varillon@gmail.com>
2023-07-12 16:25:06 +0200

    change lualine configuration

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,19 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
     config = function()
-        require("lualine").setup()
+        require("lualine").setup({
+            options = {
+                icons_enable = true,
+                theme = 'gruvbox'
+            },
+            sections = {
+                lualine_c = {
+                    {
+                        'filename',
+                        path = 1
+                    }
+                }
+            }
+        })
     end
 }
