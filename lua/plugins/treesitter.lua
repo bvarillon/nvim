2ccfdae178b6e81548ce3e1556ad71d904e69713
Benoit Varillon <benoit.varillon@gmail.com>
2023-09-21 18:31:56 +0200

    add rainbow braces

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,17 +2,23 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        'p00f/nvim-ts-rainbow'
     },
-    config = function ()
+    config = function()
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = {"c", "lua", "cpp" },
+            ensure_installed = { "c", "lua", "cpp" },
             sync_install = false,
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+            rainbow = {
+                enable = true,
+                extended_mode = true,
+                max_file_lines = nil,
+            }
         })
     end
 }
