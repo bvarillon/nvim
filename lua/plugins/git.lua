1d5b6fe8bf0685f31e97273891cb11e99f826363
bvarillon <benoit.varillon@gmail.com>
2024-07-29 11:30:57 +0200

    add git graph visualizer

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,5 +2,13 @@ return {
 	"tpope/vim-fugitive",
 	keys = {
 		{"<leader>gs", vim.cmd.Git, desc = "[G]it [S]tatus"}
-	}
+	},
+    {
+  "rbong/vim-flog",
+  lazy = true,
+  cmd = { "Flog", "Flogsplit", "Floggit" },
+  dependencies = {
+    "tpope/vim-fugitive",
+  },
+},
 }
