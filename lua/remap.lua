53a811b5b5b79b5a49d7e6da83c9e7c392eebaf0
bvarillon <benoit.varillon@gmail.com>
2025-07-23 14:14:32 +0200

    add keymaps for quicklist and lua

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,1 +1,11 @@
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", {desc = "Lua: execute line"})
+vim.keymap.set("v", "<leader>x", ":lua<CR>", {desc ="Lua: execute selection"})
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", {desc = "Lua: source current file"})
+
+print "salut"
+print "hello"
